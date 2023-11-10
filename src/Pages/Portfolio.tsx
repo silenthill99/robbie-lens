8f658018c73263d5be2349f28382fdfcaf6f99c2
silenthill99 <florian.graziani@sfr.fr>
2023-11-10 17:13:54 +0100

    Remise en page du portfolio

--- src/Pages/Portfolio.tsx
+++ src/Pages/Portfolio.tsx
@@ -4,21 +4,45 @@ export const Portfolio = () => {
         <section>
             <section>
                 <h1>Portfolio</h1>
-                <section className="bg-white">
+                <section className="bg-white p-2">
                     <h2 className="text-[#242424] mb-[80px]">Voyages</h2>
                     <div className="grid grid-cols-3 grid-rows-[300px_300px_300px] gap-[15px] relative">
-                        <a href="images/portfolio/paysage1.jpg" target="_blank" rel="noopener noreferrer relative">
+                        <a href="images/portfolio/paysage1.jpg" target="_blank" className="images relative">
                             <img className="w-[100%] h-[100%] object-cover" src="images/portfolio/paysage1.jpg" alt="" />
-                            <div>Voir la photo</div>
+                            <div className="absolute top-0 left-0 right-0 bottom-0 flex items-center justify-center text-[1.5em] bg-black/70 text-white hidden">Voir la photo</div>
                         </a>
-                        <img className="w-[100%] h-[100%] object-cover" src="images/portfolio/paysage2.jpg" alt="" />
-                        <img className="w-[100%] h-[100%] object-cover" src="images/portfolio/paysage3.jpg" alt="" />
-                        <img className="w-[100%] h-[100%] object-cover" src="images/portfolio/paysage4.jpg" alt="" />
-                        <img className="w-[100%] h-[100%] object-cover" src="images/portfolio/paysage5.jpg" alt="" />
-                        <img className="w-[100%] h-[100%] object-cover" src="images/portfolio/paysage6.jpg" alt="" />
-                        <img className="w-[100%] h-[100%] object-cover" src="images/portfolio/paysage7.jpg" alt="" />
-                        <img className="w-[100%] h-[100%] object-cover" src="images/portfolio/paysage8.jpg" alt="" />
-                        <img className="w-[100%] h-[100%] object-cover" src="images/portfolio/paysage9.jpg" alt="" />
+                        <a href="images/portfolio/paysage2.jpg" target="_blank" className="images relative">
+                            <img className="w-[100%] h-[100%] object-cover" src="images/portfolio/paysage2.jpg" alt="" />
+                            <div className="absolute top-0 left-0 right-0 bottom-0 flex items-center justify-center text-[1.5em] bg-black/70 text-white hidden">Voir la photo</div>
+                        </a>
+                        <a href="images/portfolio/paysage3.jpg" target="_blank" className="images relative">
+                            <img className="w-[100%] h-[100%] object-cover" src="images/portfolio/paysage3.jpg" alt="" />
+                            <div className="absolute top-0 left-0 right-0 bottom-0 flex items-center justify-center text-[1.5em] text-white bg-black/70 hidden">Voir la photo</div>
+                        </a>
+                        <a href="images/portfolio/paysage4.jpg" target="_blank" className="images relative">
+                            <img className="w-[100%] h-[100%] object-cover" src="images/portfolio/paysage4.jpg" alt="" />
+                            <div className="absolute top-0 left-0 right-0 bottom-0 flex items-center justify-center text-[1.5em] text-white bg-black/70 hidden">Voir la photo</div>
+                        </a>
+                        <a href="images/portfolio/paysage5.jpg" target="_blank" className="images relative">
+                            <img className="w-[100%] h-[100%] object-cover" src="images/portfolio/paysage5.jpg" alt="" />
+                            <div className="absolute top-0 left-0 right-0 bottom-0 flex items-center justify-center text-[1.5em] text-white bg-black/70 hidden">Voir la photo</div>
+                        </a>
+                        <a href="images/portfolio/paysage6.jpg" target="_blank" className="images relative">
+                            <img className="w-[100%] h-[100%] object-cover" src="images/portfolio/paysage6.jpg" alt="" />
+                            <div className="absolute top-0 left-0 right-0 bottom-0 flex items-center justify-center text-[1.5em] text-white bg-black/70 hidden">Voir la photo</div>
+                        </a>
+                        <a href="images/portfolio/paysage7.jpg" target="_blank" className="images relative">
+                            <img className="w-[100%] h-[100%] object-cover" src="images/portfolio/paysage7.jpg" alt="" />
+                            <div className="absolute top-0 left-0 right-0 bottom-0 flex items-center justify-center text-[1.5em] text-white bg-black/70 hidden">Voir la photo</div>
+                        </a>
+                        <a href="images/portfolio/paysage8.jpg" target="_blank" className="images relative">
+                            <img className="w-[100%] h-[100%] object-cover" src="images/portfolio/paysage8.jpg" alt="" />
+                            <div className="absolute top-0 left-0 right-0 bottom-0 flex items-center justify-center text-[1.5em] text-white bg-black/70 hidden">Voir la photo</div>
+                        </a>
+                        <a href="images/portfolio/paysage9.jpg" target="_blank" className="images relative">
+                            <img className="w-[100%] h-[100%] object-cover" src="images/portfolio/paysage9.jpg" alt="" />
+                            <div className="absolute top-0 left-0 right-0 bottom-0 flex items-center justify-center text-[1.5em] text-white bg-black/70 hidden">Voir la photo</div>
+                        </a>
                     </div>
                     <h2 className="text-[#242424] mb-[80px]">Portraits</h2>
                     <div className="grid grid-cols-3 grid-rows-[300px_300px] gap-[15px]">
