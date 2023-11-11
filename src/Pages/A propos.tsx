2e7bfe7baaa2323bfa1d156037f1c70898896bba
silenthill99 <florian.graziani@sfr.fr>
2023-11-11 23:33:04 +0100

    Collecteur du 10 Novembre 2023

--- src/Pages/A propos.tsx
+++ src/Pages/A propos.tsx
@@ -18,6 +18,12 @@ export const APropos = () => {
             <div>
                 <NavLink to="../portfolio" className="bg-gradient-to-r from-[#8e86b5] to-[#acaeed] text-white hover:bg-gradient-to-r hover:from-[#696484] hover:to-[#8788BA] rounded-full inline-block">VOIR MON PORTFOLIO</NavLink>
             </div>
+            <table className="text-white m-auto">
+                <caption>Tarifs</caption>
+                <tr><td className="border text-center p-2">Désignation</td><td className="border text-center p-2">Quantité</td><td className="border text-center p-2">Prix</td></tr>
+                <tr><td className="border text-center p-10">Retouches photo studio professionnel</td><td className="border text-center p-10"> x 800 photos</td><td className="border text-center p-10">3200€/800 photos</td></tr>
+                <tr><td className="border text-center p-10">Assistant photo lumière professionnel</td><td className="border text-center">x2 sets studio</td><td className="border text-center">1500€/journée</td></tr>
+            </table>
         </section>
     )
 }
