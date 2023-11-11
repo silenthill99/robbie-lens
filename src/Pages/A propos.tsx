9b38629779e5b19f33130a2f52093d0d583643a1
silenthill99 <florian.graziani@sfr.fr>
2023-11-11 23:52:28 +0100

    Correction collecteur exercices tableaux

--- src/Pages/A propos.tsx
+++ src/Pages/A propos.tsx
@@ -4,7 +4,7 @@ export const APropos = () => {
     document.title = "À propos - Robbie Lens Photographie"
     return(
         <section className="m-auto">
-            <h1>À propos</h1>
+            <h1 className="text-center">À propos</h1>
             <div className="border-y border-[#8e86b5] p-[50px]">
                 <p>Photographe depuis plus de 5 ans, je réalise des reportages aux photos dynamiques et pertinentes pour vos projets de communication. Créativité, qualité, et sérénité pour vous ! Je gère tous, depuis la direction artistique, la réalisation du reportage, jusqu'à la livraison de vos photos retouchées, prêtes à l'emploi.</p>
                 <h2>Services</h2>
@@ -18,11 +18,28 @@ export const APropos = () => {
             <div>
                 <NavLink to="../portfolio" className="bg-gradient-to-r from-[#8e86b5] to-[#acaeed] text-white hover:bg-gradient-to-r hover:from-[#696484] hover:to-[#8788BA] rounded-full inline-block">VOIR MON PORTFOLIO</NavLink>
             </div>
-            <table className="text-white m-auto">
-                <caption>Tarifs</caption>
-                <tr><td className="border text-center p-2">Désignation</td><td className="border text-center p-2">Quantité</td><td className="border text-center p-2">Prix</td></tr>
-                <tr><td className="border text-center p-10">Retouches photo studio professionnel</td><td className="border text-center p-10"> x 800 photos</td><td className="border text-center p-10">3200€/800 photos</td></tr>
-                <tr><td className="border text-center p-10">Assistant photo lumière professionnel</td><td className="border text-center">x2 sets studio</td><td className="border text-center">1500€/journée</td></tr>
+
+            <h2 className="text-center mb-20">Tarifs</h2>
+
+            <table className="text-white text-center w-[100%]">
+                <thead>
+                    <tr>
+                        <th className="border p-[15px] border-[#a5b4fc]">Désignation</th>
+                        <th className="border p-[15px] border-[#a5b4fc]">Quantité</th>
+                        <th className="border p-[15px] border-[#a5b4fc]">Prix</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td className="border border-[#a5b4fc] p-20">Retouches photo studio professionnel</td>
+                        <td className="border border-[#a5b4fc] p-20"> x 800 photos</td>
+                        <td className="border border-[#a5b4fc] p-20">3200€/800 photos</td>
+                    </tr>
+                    <tr>
+                        <td className="border border-[#a5b4fc] p-20">Assistant photo lumière professionnel</td>
+                        <td className="border border-[#a5b4fc] p-20">x2 sets studio</td>
+                        <td className="border border-[#a5b4fc] p-20">1500€/journée</td></tr>
+                </tbody>
             </table>
         </section>
     )
